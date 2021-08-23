e736a121de25b487ddb30b47979d6ab8f361b58c
LucasDanielBCCIFGoiano <88989934+LucasDanielBCCIFGoiano@users.noreply.github.com>
2021-08-23 01:03:32 -0300

    Adicionando os arquivos
    Lucas Daniel da Silva.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulamentoCinaraLucasDaniel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro Astra = new Carro("Astra");
+            Astra.Ligar();
+            Astra.Mover();
+            Astra.Abastecer();
+            Astra.Mover();
+            Console.ReadLine();
+        }
+    }
+}
